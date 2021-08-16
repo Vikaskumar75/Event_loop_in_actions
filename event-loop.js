547655ccd022ef44b70e1e7348228253a8cf277c
Vikas <vikas@bigheadsoftwares.com>
2021-08-17 00:08:49 +0530

    code blocking and event loop

--- event-loop.js
+++ event-loop.js
@@ -1,16 +1,55 @@
-const fs = require('fs');
+const fs = require("fs");
+const crypto = require("crypto");
 
-setTimeout(() =>   console.log("Timer 1 finished") , 0);
-setTimeout(() =>   console.log('Timer 2 finished') , 3000);
+const start = Date.now();
 
-setImmediate(()=>console.log('Immediate 1 is finished'));
+process.env.UV_THREADPOOL_SIZE = 4;
 
-fs.readFile('test-file.txt', () => {
-    console.log('fileRead')
-    setTimeout(() =>   console.log('Timer 3 finished') , 0);
-setTimeout(() =>   console.log('Timer 4 finished') , 3000);
+console.log(start);
+setTimeout(() => console.log("Timer 1 finished"), 0);
+setTimeout(() => console.log("Timer 2 finished"), 3000);
 
-setImmediate(()=>console.log('Immediate 2 is finished'));
+setImmediate(() => console.log("Immediate 1 is finished"));
+
+fs.readFile("test-file.txt", () => {
+  console.log("fileRead");
+  console.log(
+    "----------------------------------------------------------------"
+  );
+  setTimeout(() => console.log("Timer 3 finished"), 0);
+  setTimeout(() => console.log("Timer 4 finished"), 3000);
+
+  setImmediate(() => console.log("Immediate 2 is finished"));
+
+  process.nextTick(() => {
+    console.log("process.nextTick finished");
+  });
+
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  console.log(Date.now() - start, "Password is encrypted");
+
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  console.log(Date.now() - start, "Password is encrypted");
+
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  console.log(Date.now() - start, "Password is encrypted");
+
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  console.log(Date.now() - start, "Password is encrypted");
+
+  //   crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+  //     console.log(Date.now() - start, "Password is encrypted");
+  //   });
+
+  //   crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+  //     console.log(Date.now() - start, "Password is encrypted");
+  //   });
+  //   crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+  //     console.log(Date.now() - start, "Password is encrypted");
+  //   });
+  //   crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+  //     console.log(Date.now() - start, "Password is encrypted");
+  //   });
 });
 
-console.log('this is top level code');
+console.log("this is top level code");
